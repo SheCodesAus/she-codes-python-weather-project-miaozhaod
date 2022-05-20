1993e494017dcd3ef23c7faa417f6fa6882c4844
Miao Zhao <miao_zhao@yahoo.com>
2022-05-20 21:48:05 +1000

    Complete `convert_date`, almost passed `generate_summary`, still need to find out the lowest temp's location and date issue for `example_three`

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+from audioop import avg
 import csv
 from datetime import datetime
 
@@ -17,17 +18,16 @@ def format_temperature(temp):
 
 
 def convert_date(iso_string):
-    """Converts and ISO formatted date into a human readable format.
+    """Converts an ISO formatted date into a human readable format.
 
     Args:
         iso_string: An ISO date string..
     Returns:
         A date formatted like: Weekday Date Month Year e.g. Tuesday 06 July 2021
     """
-    # strptime
-    # strftime
-    date_object = datetime.strptime(iso_string, "%d %B, %Y")
-    return date_object
+    date = datetime.strptime(iso_string, "%Y-%m-%dT%H:%M:%S%z").strftime("%A %d %B %Y")
+    return date
+    # done
 
 
 def convert_f_to_c(temp_in_farenheit):
@@ -138,8 +138,73 @@ def generate_summary(weather_data):
     Returns:
         A string containing the summary information.
     """
-    for weather in weather_data:
-        return weather
+    # list = []
+    # with open(weather_data) as csv_file:
+    #     reader = csv.reader(csv_file)
+    #     for line in reader:
+    #         if line != []:
+    #             list.append(line)
+    # list.pop(0)
+    # listResult = []
+    # for li in list:
+    #     num = [li[0], (int(li[1])), (int(li[2]))]
+    #     listResult.append(num)
+
+    weathers = []
+    for li in weather_data:
+        date = datetime.strptime(li[0], "%Y-%m-%dT%H:%M:%S%z").strftime("%A %d %B %Y")
+        low = round((float(li[1]) - 32) * 5 / 9, 1)
+        high = round((float(li[2]) - 32) * 5 / 9, 1)
+        newLi = [date, low, high]
+        weathers.append(newLi)
+    # print("weathers: ", weathers) # got a list of weathers[] in the format of date, low, high
+    days = len(weathers);
+    
+    low_temps = []
+    for weather in weathers:
+        low_temps.append(weather[1])
+    # print("low_temps: ", low_temps) # got a list of the low_temps[] in the above weathers[]
+    avg_low = round(sum(low_temps) / len(low_temps), 1)
+    # print("avg_low: ", avg_low)
+
+    min_low = float(low_temps[0])
+    min_low_location = 0
+    index = 0
+    for low_temp in low_temps:
+        if float(low_temp) <= min_low:
+            min_low = float(low_temp)
+            min_low_location = index
+            index += 1
+    date_of_min_low = weathers[min_low_location][0]
+    # print("min_low | min_low_location | date_of_min_low: ", min_low, "|", min_low_location, "|", date_of_min_low)
+
+    high_temps = []
+    for weather in weathers:
+        high_temps.append(weather[2])
+    # print("high_temps: ", high_temps)  # got a list of the low_temps[] in the above weathers[]
+    avg_high = round(sum(high_temps) / len(high_temps), 1)
+    # print("avg_high: ", avg_high)
+
+    max_high = float(high_temps[0])
+    max_high_location = 0
+    index = 0
+    for high_temp in high_temps:
+        if float(high_temp) >= max_high:
+            max_high = float(high_temp)
+            max_high_location = index
+            index += 1
+    date_of_max_high = weathers[max_high_location][0]
+    # print("max_high | max_high_location | date_of_max_high: ", max_high, "|", max_high_location, "|", date_of_max_high)
+
+    summary_heading = f"{days} Day Overview"
+    lowest_summary = f"The lowest temperature will be {min_low}°C, and will occur on {date_of_min_low}."
+    highest_summary = f"The highest temperature will be {max_high}°C, and will occur on {date_of_max_high}."
+    avg_low_summary = f"The average low this week is {avg_low}°C."
+    avg_high_summary = f"The average high this week is {avg_high}°C."
+    summary_result = f"{summary_heading}\n  {lowest_summary}\n  {highest_summary}\n  {avg_low_summary}\n  {avg_high_summary}\n"
+    return summary_result
+
+# print(generate_summary("./tests/data/example_one.csv"))
 
 
 def generate_daily_summary(weather_data):
